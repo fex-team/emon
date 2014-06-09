0b9bf41c6605172588a7d0e8ebd225e5afffc69f
campaign <zhanyi1022@gmail.com>
2014-06-09 14:20:16 +0800

    core添加

---
+++ import.js
@@ -0,0 +1,26 @@
+/**
+ * 开发版本的文件导入
+ */
+(function () {
+    var paths = [
+        'core/emon.js',
+        'core/Class.js',
+        'core/utils.js',
+        'core/editor.js',
+        'core/command.js',
+        'core/module.js',
+        'core/event.js',
+        'core/editor.event.js',
+        'core/editor.module.js',
+        'core/editor.command.js',
+        'core/keymap.js',
+        'core/editor.lang.js',
+        'core/editor.defaultoptions.js',
+        'core/browser.js'
+
+    ],
+    baseURL = 'src/', doc = document;
+    while (paths.length) {
+        doc.write('<script type="text/javascript" src="' + baseURL + paths.shift() + '"></script>');
+    }
+})();
